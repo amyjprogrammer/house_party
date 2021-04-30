7e498a6682f3767f2c3ebe7a52a0f10329729411
amyjprogrammer <amyjprogrammer@gmail.com>
2021-04-30 19:14:35 -0400

    More Modifications to Room and Home Page w/new View

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -11,6 +11,7 @@ export default class HomePage extends Component {
         this.state = {
             roomCode: null,
         };
+        this.clearRoomCode = this.clearRoomCode.bind(this);
     }
 
     async componentDidMount() {
@@ -20,6 +21,12 @@ export default class HomePage extends Component {
             this.setState({
                 roomCode: data.code
             });
+        });
+    }
+
+    clearRoomCode(){
+        this.setState({
+            roomCode: null,
         });
     }
 
@@ -60,7 +67,13 @@ export default class HomePage extends Component {
                     />       
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
-                    <Route path='/room/:roomCode' component={Room} />
+                    <Route 
+                        path='/room/:roomCode' 
+                        render={(props) => {
+                            return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                        }}
+                        
+                    />
                 </Switch>
             </Router>
         );
